bf0a1b1d4dc02a4fde0e70e060c8eb68e9c9db08
Jesus David <jdrch62@gmail.com>
2024-11-08 11:14:46 -0400

    Filters advances

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -14,31 +14,71 @@ const MainContent: React.FC  = () => {
                 <div className="select-game-filter">
                     <div className="select-game-filter-first-part">
                         <img className="sword-img" src="/sword.png"></img>
-                        Select a game  
+                        <div className="custom-select">
+                                <select style={{ height: '25px' }}>
+                                    <option value="All" selected>Select a game</option>
+                                    <option value="game1">Game 1</option>
+                                    <option value="game2">Game 2</option>
+                                    <option value="game3">Game 3</option>
+                                    <option value="game4">Game 4</option>
+                                    <option value="game5">Game 5</option>
+                                </select>
+                            </div>
                     </div>
                     <div className="select-game-filter-second-part">
-                        <FontAwesomeIcon icon={faCaretDown} />
+                        <FontAwesomeIcon className="caret-down" icon={faCaretDown} />
                     </div>
                 </div>
 
                 <div className="search-filter">
                     <div className="search-filter-inside">
                         <FontAwesomeIcon icon={faMagnifyingGlass} />
-                        <input type="text" placeholder="search" style={{backgroundColor:'transparent', border: 'none', marginLeft: '15px'}}/>
+                        <input type="text" className="search-filter-inside-input" placeholder="search"/>
                     </div>
                 </div>
 
                 <div className="price-filter">
-                    <div className="search-filter-inside">
-                        <FontAwesomeIcon icon={faSackDollar} />
-                        Price
+                    <div className="price-filter-inside">
+                        <FontAwesomeIcon className="dolar" icon={faSackDollar} />
+                        <div className="custom-select">
+                            <div style={{display:'grid', alignItems:'center'}}>
+                                Price
+                                <select style={{height:'25px'}}>
+                                    <option value="All" selected>All</option>
+                                    <option value="0-10">$0 - $10</option>
+                                    <option value="10-20">$10 - $20</option>
+                                    <option value="20-50">$20 - $50</option>
+                                    <option value="50-100">$50 - $100</option>
+                                    <option value="100+">$100+</option>
+                                </select>
+                            </div>
+                            <FontAwesomeIcon className="caret-icon" icon={faCaretDown} />
+                        </div>
                     </div>
                 </div>
 
                 <div className="item-filter">
-                    <div className="search-filter-inside">
-                        <FontAwesomeIcon icon={faFeather} />
-                        Item type
+                    <div className="item-filter-inside">
+                        <FontAwesomeIcon className="feather" icon={faFeather} />
+                        <div className="custom-select">
+                            <div style={{display:'grid', alignItems:'center'}}>
+                                Item type
+                                <select style={{height:'25px'}}>
+                                    <option value="All" selected>All</option>
+                                    <option value="Weapons">Weapons</option>
+                                    <option value="Armor">Armor</option>
+                                    <option value="Consumables">Consumables</option>
+                                    <option value="Crafting Materials">Crafting Materials</option>
+                                    <option value="Quest Items">Quest Items</option>
+                                    <option value="Accessories">Accessories</option>
+                                    <option value="Magic Items">Magic Items</option>
+                                    <option value="Tools">Tools</option>
+                                    <option value="Treasure">Treasure</option>
+                                    <option value="Miscellaneous">Miscellaneous</option>
+                                </select>
+                            </div>
+                            <FontAwesomeIcon className="caret-icon" icon={faCaretDown} />
+                        </div>
                     </div>
                 </div>
             </div>
