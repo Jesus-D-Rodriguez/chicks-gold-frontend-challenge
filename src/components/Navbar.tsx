0d234e38ec37489000c9d72f23d90fde51f4a888
jesus <jdrch62@gmail.com>
2024-11-09 23:17:29 -0400

    advances

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,18 @@
 import "../styles/Navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faCartShopping, faUser, faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 
 const Navbar: React.FC = () => {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
     return (
     <div className="navbar">
         <div className="navbar-container">
+    
             <div className="logo-container">
                 <img className="logo" src="chicks-logo-large.svg" alt="Chicks Gold Inc Logo"></img>
             </div>
@@ -24,7 +31,38 @@ const Navbar: React.FC = () => {
                     <button className="sign-in-button">SIGN IN <FontAwesomeIcon icon={faUser} /></button>
                 </div>
             </div>
+            <div className="mobile-actions">
+                <div style={{display:'flex'}}>
+                    <button className="mobile-menu-icon" style={{marginRight:'15px'}} onClick={toggleMobileMenu}>
+                        <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
+
+                    </button>
+                    <div className="logo-container-mobile">
+                        <img className="logo" src="chicks-logo-large.svg" alt="Chicks Gold Inc Logo"></img>
+                    </div>
+                </div>
+                    
+                    <button className="sign-in-button mobile-sign-in">SIGN IN <FontAwesomeIcon icon={faUser} /></button>
+                </div>
         </div>
+
+        {isMobileMenuOpen && (
+                <div className="mobile-menu">
+                    <button className="close-mobile-menu" onClick={toggleMobileMenu}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </button>
+                    <div className="mobile-menu-content">
+                        <a className="options" href="">CURRENCY</a> 
+                        <a className="options" href="">ITEMS</a>
+                        <a className="options" href="">ACCOUNTS</a>
+                        <a className="options" href="">SERVICES</a>
+                        <a className="options" href="">SWAP</a>
+                        <a className="options" href="">SELL</a>
+                        <a className="options" href="">USD</a>
+                        <a className="options" href=""><FontAwesomeIcon className="cart-icon" icon={faCartShopping} /> CART (5)</a>
+                    </div>
+                </div>
+            )}
     </div>);
 }
 
